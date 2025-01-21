a5af6039b53723762626c7b4e2f0e3a24fd77d06
rahulkfernandes <rahulkfernandes@gmail.com>
2025-01-21 02:08:15 -0500

    data types, select, filter

--- basics.R
+++ basics.R
@@ -58,3 +58,42 @@ unique(msleep$vore)
 missing <- !complete.cases(msleep)
 msleep[missing,]
 
+starwars %>% 
+  select(name, height, mass)
+
+starwars %>% 
+  select(1:3)
+
+starwars %>% 
+  select(ends_with("color"))
+
+starwars %>% 
+  select(name, mass, height, everything())
+
+starwars %>% 
+  rename("character" = "name") %>% 
+  View()
+
+class(starwars$hair_color)
+
+starwars$hair_color <- as.factor(starwars$hair_color)
+class(starwars$hair_color)
+
+starwars %>% 
+  mutate(hair_color = as.character(hair_color)) %>% 
+  glimpse()
+
+df <- starwars
+
+df$sex <- as.factor(df$sex)
+
+levels(df$sex)
+
+df <- df %>% 
+  mutate(sex=factor(sex, levels=c("male", "female", "hermaphroditic", "none")))
+
+levels(df$sex)
+
+starwars %>% 
+  select(name, mass, sex) %>% 
+  filter(mass < 55 & sex == "male")
